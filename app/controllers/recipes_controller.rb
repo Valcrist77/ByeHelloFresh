d9654a8df633fed52927ac5ee6e2c9ef30099601
David Bai <valcrist77@gmail.com>
2021-04-29 21:40:32 -0700

    add some basic views + ingredient model

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,12 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
   end
 
   def create
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_041028) do
+ActiveRecord::Schema.define(version: 2021_04_30_044006) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
